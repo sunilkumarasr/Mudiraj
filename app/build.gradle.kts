acf597a872541ee9f863efceb1344503ee2b8ea1
sunil k <32087491+sunilkumarasr@users.noreply.github.com>
2025-05-30 16:50:17 +0530

    30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mudiraj.mudirajfoundation"
         minSdk = 25
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
